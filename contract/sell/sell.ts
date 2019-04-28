942426cbf8e83cf467a4daa5a46f106807329368
flyq <flyq951@gmail.com>
2019-04-28 09:34:20 +0800

    Update sell.ts

--- contract/sell/sell.ts
+++ contract/sell/sell.ts
@@ -1,4 +1,4 @@
-import { AssertionError } from "assert";
+import { constants } from "zlib";
 
 const DEV = 'ADXEbSc8N8yV76oA4hvKDBFtiqumsjTT5a'
 const InitName = '妖灵蛋'
@@ -64,6 +64,7 @@ export class Idol extends AschContract {
     userToOwnedId: Mapping<Vector<string>>
 
     onsellToId: Vector<string>
+    idToAuction: Mapping<Auction>
 
     constructor() {
         super()
@@ -72,6 +73,7 @@ export class Idol extends AschContract {
         this.idToIdol = new Mapping<IdolBase>()
         this.userToOwnedId = new Mapping<Vector<string>>()
         this.onsellToId = new Vector<string>()
+        this.idToAuction = new Mapping<Auction>()
     }
 
     newIdol() {
@@ -82,14 +84,66 @@ export class Idol extends AschContract {
         this.idolAmount++
 
         this.userToOwnedId[DEV].push(_id)
-        
-
     }
 
     @payable
     buyIdol(amount: bigint, currency: string, _id: string) : void {
-        assert()
+        assert(amount >= this.idToAuction[_id].startPrice)
+        this.idToAuction[_id].owner = this.context.senderAddress
     }
 
-    sellIdol(_id: string, _startPrice: )
+    sellIdol(_id: string, _startPrice: bigint, _endingPrice: bigint, _duration: number) : void {
+        assert(this.context.senderAddress === idToIdol[_id].owner)  // idol 存在且是owner在操作
+        assert(idToIdol[_id].boolean === false) // idol 不能处于出售状态
+        this.isOnsellAmount++
+        this.onsellToId.push(_id)
+        this.idToAuction[_id] = new Auction(this.context.senderAddress, _startPrice, _endingPrice, _duration, 0)
+    }
+
+    unlockEgg(_id: string, _address: string) : void {
+        this.idToIdol[_id].address = _address
+    }
+
+    @constant
+    getCurrentPrice(_id: string) : BigInt {
+        const _startPrice = this.idToAuction[_id].startPrice
+
+        return _startPrice
+    } 
+
+    @constant
+    getIdolInfo(_id: string) : IdolBaseInfo {
+        const _owner = this.idToIdol[_id].owner
+        const _state = this.idToIdol[_id].state
+        const _name = this.idToIdol[_id].name
+        const _intro = this.idToIdol[_id].intro
+        const _address = this.idToIdol[_id].address
+        const _isOnsell = this.idToIdol[_id].isOnsell
+
+        return {_id, _owner, _state, _name, _intro, _address, _isOnsell}
+    }
+
+    @constant
+    getAuctionInfo(_id: string) : AuctionInfo {
+        const _seller = this.idToAuction[_id].seller
+        const _startPrice = this.idToAuction[_id].startPrice
+        const _endPrice = this.idToAuction[_id].endPrice
+        const _duration = this.idToAuction[_id].duration
+        const _startedAt = this.idToAuction[_id].startedAt
+
+        return { _seller, _startPrice, _endPrice, _duration, _startedAt }
+    }
+
+    @constant
+    getTotal() : number {
+        const _amount = this.idolAmount
+
+        return _amount
+    }
+
+    @constant
+    getSellAmount() : number {
+        const _amount = this.isOnsellAmount
+        return _amount
+    }
 }
